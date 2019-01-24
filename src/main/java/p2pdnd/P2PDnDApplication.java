eebb0a4968de6ed3ae79df42fd73dbde11a27d3f
igor <nemykin@i-teco.ru>
2019-01-24 11:42:24 +0300

    каркас проги. первый комит

---
+++ src/main/java/p2pdnd/P2PDnDApplication.java
@@ -0,0 +1,20 @@
+package p2pdnd;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+
+/**
+ * @author igor 24.01.19
+ */
+@Slf4j
+public class P2PDnDApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public void run(String... args) {
+
+    }
+}
